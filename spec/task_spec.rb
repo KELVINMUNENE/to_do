7b011880e4866a8a04b5dc2bad0169afc264a2f8
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-10 11:11:27 +0300

    added a task method

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -12,6 +12,36 @@ DB = PG.connect({:dbname => 'to_do_test'})
 
 
 
+  describe(Task) do
+      describe("#==") do
+        it("is the same task if it has the same description") do
+          task1 = Task.new({:description => "learn SQL"})
+          task2 = Task.new({:description => "learn SQL"})
+          expect(task1).to(eq(task2))
+        end
+      end
+    end
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 describe(Task) do
