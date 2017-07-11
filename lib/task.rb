49262b2189db7c3eb899926d1c7bee821d53188d
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-11 09:03:07 +0300

    executed databse in my singleton method

--- lib/task.rb
+++ lib/task.rb
@@ -1,27 +1,34 @@
 class Task
-    attr_reader(:description, :list_id)
+    @@all_tasks = []
 
-    define_method(:initialize) do |attributes|
-      @description = attributes.fetch(:description)
-      @list_id = attributes.fetch(:list_id)
+    define_method(:initialize) do |description|
+      @description = description
+    end
+
+    define_method(:description) do
+      @description
     end
 
     define_singleton_method(:all) do
-      returned_tasks = DB.exec("SELECT * FROM tasks;")
-      tasks = []
-      returned_tasks.each() do |task|
-        description = task.fetch("description")
-        list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
-        tasks.push(Task.new({:description => description, :list_id => list_id}))
-      end
-      tasks
+      @@all_tasks
     end
 
     define_method(:save) do
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
-  end
+      @@all_tasks.push(self)
+    end
 
-    define_method(:==) do |another_task|
-      self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+    define_singleton_method(:clear) do
+      returned_tasks = DB.exec("SELECT * FROM tasks;")
+      tasks = []
+      returned_tasks.each()do |task|
+        description = task.fetch("description")
+        tasks.push(Task.new({:description => description}))
+      end
+      tasks
+
+    end
+
+    define_method(:==)do |another_task|
+        self.description( ).==(another_task.description())
     end
 end
